8e25898f14b9a7afe11c819f3f5dad04272dc93a
Tanishqa24xx <sgakude@gmail.com>
2026-03-21 13:17:26 +0800

    Delete weblink-scanner directory

--- weblink-scanner/app/src/main/java/com/example/linkscanner/model/ForgotPasswordResponse.kt
+++
@@ -1,5 +0,0 @@
-package com.example.linkscanner.model
-
-data class ForgotPasswordResponse(
-    val message: String?
-)
--- weblink-scanner/app/src/main/java/com/example/linkscanner/model/SignupResponse.kt
+++
@@ -1,7 +0,0 @@
-package com.example.linkscanner.model
-
-data class SignupResponse(
-    val message: String?,
-    val email: String?,
-    val plan: String?
-)
